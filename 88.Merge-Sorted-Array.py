ea294e99f62d8f5f0fc3eb46919bcdd4c82e132c
Satvik-Creations <satviksinghal07@gmail.com>
2026-09-04 00:31:20 +0530

    88.Merge-Sorted-Array.py

---
+++ 88.Merge-Sorted-Array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        del nums1[m:]
+        nums1.extend(nums2)
+        nums1.sort()
+
+print(Solution().merge([1,2,3,0,0,0], 3, [2,5,6], 3)) #[1,2,2,3,5,6]
