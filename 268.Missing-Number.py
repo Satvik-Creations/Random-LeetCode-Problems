454a5f664a2485d03754f5afd7059cccbdf072a1
Satvik-Creations <satviksinghal07@gmail.com>
2026-09-06 00:57:47 +0530

    268.Missing-Number.py

---
+++ 268.Missing-Number.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        l = len(nums)
+        nums.sort()
+        for j in range(0,l+1):
+            if j not in nums:
+                return j
+            else:
+                ...
+
+# print(Solution().missingNumber([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39]))
+# print(Solution().missingNumber([3,0,1]))
