a240008a7257665f80817054faf68a5885591d0a
Satvik-Creations <satviksinghal07@gmail.com>
2026-09-10 19:34:08 +0530

    627.Swap-Sex-of-Employees.sql

---
+++ 627.Swap-Sex-of-Employees.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+UPDATE Salary SET sex = CASE
+    WHEN sex = "f" THEN "m"
+    WHEN sex = "m" THEN "f"
+END;
+
+/*
+
+Input:
+
+| id | name | sex | salary |
+| -- | ---- | --- | ------ |
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
+
+Output:
+
+| id | name | sex | salary |
+| -- | ---- | --- | ------ |
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
+
+
+*/
