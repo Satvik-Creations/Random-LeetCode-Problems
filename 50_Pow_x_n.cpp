eb0fabf2c69ecf5500c8cfe8c3567221f438d8cc
Satvik-Creations <satviksinghal07@gmail.com>
2026-09-19 23:08:15 +0530

    50_Pow_x_n.cpp

--- 50_Pow_x_n.cpp
+++ 50_Pow_x_n.cpp
@@ -1,17 +1,32 @@
-#include <iostream>
-#include <cmath>
-
-using namespace std;
 class Solution {
 public:
     double myPow(double x, int n) {
-        return std::pow(x,n);
+
+        // General/Basic Cases
+        if (x == 0) return 0.0;
+        if (x == 1) return 1.0;
+        if (n == 0) return 1.0;
+        if (x == -1 && n%2 == 0) return 1.0;
+        if (x == -1 && n%2 != 0) return -1.0;
+
+        long BinForm = n;
+        double ans = 1;
+
+        // Negative Case Handling
+        if (n<0) {
+            x = 1/x;
+            BinForm = -BinForm;
+        }
+
+        // Positive Case Handling
+        while (BinForm > 0) {
+            if (BinForm %2 == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+            BinForm /= 2;
+        }
+        return ans;
     }
 };
-
-// int main() {
-//     Solution sol;
-//     double result = sol.myPow(2.0,3);
-//     cout << result << endl;
-//     return 0;
-// }
