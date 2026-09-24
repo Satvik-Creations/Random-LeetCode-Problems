3d54dfb3c8e806f716096c5da654c464d1f8ecf6
Satvik-Creations <satviksinghal07@gmail.com>
2026-09-24 19:55:26 +0530

    175.Combine-Two-Tables.sql

---
+++ 175.Combine-Two-Tables.sql
@@ -0,0 +1,33 @@
+-- # Write your MySQL query statement below
+
+SELECT Person.firstName, Person.lastName, Address.city, Address.state FROM Person
+LEFT JOIN Address ON Person.personID = Address.personID;
+
+
+/*
+
+Input:
+
+Person = 
+
+| personId | lastName | firstName |
+| -------- | -------- | --------- |
+| 1        | Wang     | Allen     |
+| 2        | Alice    | Bob       |
+
+Address =
+
+| addressId | personId | city          | state      |
+| --------- | -------- | ------------- | ---------- |
+| 1         | 2        | New York City | New York   |
+| 2         | 3        | Leetcode      | California |
+
+Output:
+
+| firstName | lastName | city          | state    |
+| --------- | -------- | ------------- | -------- |
+| Allen     | Wang     | null          | null     |
+| Bob       | Alice    | New York City | New York |
+
+
+*/
